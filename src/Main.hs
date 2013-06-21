3974f7a3bf5d14b0f9ed144dae7cb20d56de67b8
Michael Snoyman <michael@fpcomplete.com>
2013-06-21 07:14:52 +0000

    Github Gist upload

--- src/Main.hs
+++ src/Main.hs
@@ -10,17 +10,26 @@ import           Data.List                  (sortBy)
 import           Data.Monoid
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
+import qualified Data.Text.Lazy             as TL
 import           Data.Text.Lazy.Builder
 import           Data.Text.Lazy.Builder.Int
-import qualified Data.Text.Lazy.IO          as TLIO
+import qualified Data.Text.IO          as TIO
 import           Data.Time                  (UTCTime, getCurrentTime)
 import           Data.Word                  (Word)
 import           Network.HTTP.Conduit
+import Data.Aeson
+import qualified Data.ByteString.Char8 as S8
+import Control.Monad.IO.Class (liftIO)
+import Data.Text.Lazy.Encoding (decodeUtf8With)
+import Data.Text.Encoding.Error (lenientDecode)
+import qualified Data.HashMap.Strict as HashMap
 
 -- | The main entry point.
 main :: IO ()
 main = forever $ do
+    putStrLn "Starting parse"
     parseLog
+    putStrLn "Sleeping"
     threadDelay $ 1000 * 1000 * 60 * 30 -- 30 minutes
 
 data UploadStats = UploadStats
@@ -43,13 +52,40 @@ parseLog :: IO ()
 parseLog = do
     now <- getCurrentTime
     req <- parseUrl "http://hackage.haskell.org/packages/archive/log"
-    us <- withManager $ \man -> do
+    withManager $ \man -> do
         res <- http req man
-        responseBody res
+        us <- responseBody res
             $$+- CT.decode CT.utf8
              =$ CT.lines
              =$ CL.fold addUpload mempty
-    TLIO.putStr $ toLazyText $ prettyUploadStats now us
+        liftIO $ putStrLn "Parse succeeded, uploading"
+        let upload = def
+                { method = "POST"
+                , secure = True
+                , host = "api.github.com"
+                , port = 443
+                , path = "/gists"
+                , requestBody = RequestBodyLBS $ encode $ object
+                    [ "description" .= ("Hackage upload log @ " ++ show now)
+                    , "public" .= True
+                    , "files" .= object
+                        [ "upload-log.txt" .= object
+                            [ "content" .= toLazyText (prettyUploadStats now us)
+                            ]
+                        ]
+                    ]
+                , requestHeaders =
+                    [ ("User-Agent", "http-conduit")
+                    ]
+                }
+        res2 <- httpLbs upload man
+        liftIO $ TIO.putStrLn $ case decode $ responseBody res2 of
+            Just (Object o)
+              | Just (String htmlUrl) <- HashMap.lookup "html_url" o ->
+                "Uploaded to: " <> htmlUrl
+              | otherwise -> "Couldn't find html_url: " <> T.pack (show o)
+            Nothing -> "Invalid JSON response: " <> TL.toStrict (decodeUtf8With lenientDecode $ responseBody res2)
+                
   where
     addUpload us = mappend us . parseUpload
     parseUpload t =
